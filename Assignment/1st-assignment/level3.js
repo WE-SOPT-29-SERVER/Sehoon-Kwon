d3d1374c5d0e32cbba201a9136478afb9309607f
hoon <devkwonsehoon@naver.com>
2021-10-06 16:52:10 +0900

    [add] 과제추가 및 폴더링

---
+++ Assignment/1st-assignment/level3.js
@@ -0,0 +1,9 @@
+/**
+ * @LEVEL3
+ * @desc 서버파트 members.js 데이터를 이용해서 랜덤으로 조를 짜는 알고리즘 만들어보기
+ * @desc 조건: OB, YB 비율 오차범위를 최소한으로 유지하며 코드 작성
+ */
+
+const members = require('./members');
+
+console.log(members);
