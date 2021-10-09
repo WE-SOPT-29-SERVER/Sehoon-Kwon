5636096663350061afd205fb452f2194b8c47fb8
hoon <devkwonsehoon@naver.com>
2021-10-09 16:17:18 +0900

    [add] module 실습

---
+++ Seminar/2nd-seminar/calculator.js
@@ -0,0 +1,16 @@
+const cal = {
+  sum: (a, b) => {
+    return console.log(a + b);
+  },
+  subtract: (a, b) => {
+    return console.log(a - b);
+  },
+  multiply: (a, b) => {
+    return console.log(a * b);
+  },
+  divide: (a, b) => {
+    return console.log(a / b);
+  },
+};
+
+module.exports = cal;
---
+++ Seminar/2nd-seminar/main.js
@@ -0,0 +1,6 @@
+const cal = require('./calculator');
+
+cal.sum(1, 3);
+cal.subtract(3, 1);
+cal.divide(1, 3);
+cal.multiply(1, 3);
