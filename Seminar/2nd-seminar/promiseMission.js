b817d243dc83506bb5d4789690c7d06e13f4bccf
hoon <devkwonsehoon@naver.com>
2021-10-09 15:41:27 +0900

    [update] consolle 출력

--- Seminar/2nd-seminar/promiseMission.js
+++ Seminar/2nd-seminar/promiseMission.js
@@ -3,6 +3,7 @@ const members = require('./members');
 const getOnline = members => {
   return new Promise((res, err) => {
     setTimeout(() => {
+      console.log('...온라인 인원 필터링...');
       const onlineMembers = members.filter(
         member => member.location === 'online'
       );
@@ -14,6 +15,7 @@ const getOnline = members => {
 const getOffline = members => {
   return new Promise((res, err) => {
     setTimeout(() => {
+      console.log('...오프라인 인원 필터링...');
       const offlineMembers = members.filter(
         member => member.location === 'offline'
       );
@@ -25,6 +27,7 @@ const getOffline = members => {
 const getYB = members => {
   return new Promise((res, err) => {
     setTimeout(() => {
+      console.log('...YB 인원 필터링...');
       const membersYB = members.filter(member => member.group === 'YB');
       res(membersYB);
     }, 500);
@@ -34,6 +37,7 @@ const getYB = members => {
 const getOB = members => {
   return new Promise((res, err) => {
     setTimeout(() => {
+      console.log('...OB 인원 필터링...');
       const membersOB = members.filter(member => member.group === 'OB');
       res(membersOB);
     }, 500);
