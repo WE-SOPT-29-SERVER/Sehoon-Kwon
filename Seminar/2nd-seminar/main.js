b26f87a6548c75be36905d12ef2c230b74726568
hoon <devkwonsehoon@naver.com>
2021-10-09 16:22:22 +0900

    [update] Mission 수정

--- Seminar/2nd-seminar/main.js
+++ Seminar/2nd-seminar/main.js
@@ -1,11 +1,11 @@
-const sum = require('./sum');
-const cal = require('./calculator');
+// const sum = require('./sum');
+const { sum, subtract, divide, multiply } = require('./calculator');
 
-console.log(`\n===== sum module =====`);
-sum(2, 5);
+// console.log(`\n===== sum module =====`);
+// sum(2, 5);
 
 console.log(`\n===== cal module =====`);
-cal.sum(1, 3);
-cal.subtract(3, 1);
-cal.divide(1, 3);
-cal.multiply(1, 3);
+sum(1, 3);
+subtract(3, 1);
+divide(1, 3);
+multiply(1, 3);
